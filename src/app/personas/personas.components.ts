fac5be94d746aef8bd24ba887eae252a4f8c1b53
Sebastian Martínez <35276965+smartinez05@users.noreply.github.com>
2021-11-14 15:35:41 -0500

    24 two way binding

--- src/app/personas/personas.components.ts
+++ src/app/personas/personas.components.ts
@@ -16,7 +16,7 @@ export class PersonasComponent {
       this.mensaje = 'Persona agregada';
     }
   }
-  modificarTitulo(event: Event) {
+  /* modificarTitulo(event: Event) {
     this.titulo = (<HTMLInputElement>event.target).value;
-  }
+  } */
 }
