db506deecd203328d3fe119bbc452ce42fb9632c
Sebastian Martínez <35276965+smartinez05@users.noreply.github.com>
2021-11-14 14:10:44 -0500

    revertir componentes inline

--- src/app/personas/personas.components.ts
+++ src/app/personas/personas.components.ts
@@ -2,9 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-personas',
-    template: `<h1>Listado personas</h1>
-    <app-persona></app-persona>
-    <app-persona></app-persona>`
+    templateUrl: './personas.component.html'
 })
 export class PersonasComponent {
     
