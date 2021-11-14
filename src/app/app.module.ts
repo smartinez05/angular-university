cdaa5c6854c7941b9394765b2bf440b1263d4f39
Sebastian Martínez <35276965+smartinez05@users.noreply.github.com>
2021-11-14 13:09:47 -0500

    14 importar componente personas en app-root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonasComponent } from './personas/personas.components';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, PersonasComponent
   ],
   imports: [
     BrowserModule
